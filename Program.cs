926dc8cd52b2abbec7c604a49dd52e6982f8329d
alenazharina <107620858+alenazharina@users.noreply.github.com>
2022-06-17 11:14:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray;
+
+            myarray = new int[5];
+
+            myarray[1] = 3;
+
+            myarray[4] = 10;
+
+            long[] myarray1;
+
+            myarray1 = new long[5];
+
+            myarray1[2] = 7;
+
+            myarray1[4] = 12;
+
+            float[] myarray2;
+
+            myarray2 = new float[5];
+
+            myarray2[3] = 3;
+
+            myarray2[4] = 9;
+
+               
+
+            Console.WriteLine ("Result = {0}", myarray[0]);
+        }
+    }
+}
