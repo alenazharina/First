1068426de546560c3694610f85e77ec50c43de92
Alena Zharina <alenazharina@coherentsolutions.com>
2022-06-21 17:44:19 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -6,33 +6,43 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            int[] myarray;
+            int[] myarray = new int[3];
 
-            myarray = new int[5];
+            myarray[0] = 1;
 
-            myarray[1] = 3;
+            myarray[1] = myarray[0] * myarray[0];
 
-            myarray[4] = 10;
+            myarray[2] = myarray[0] + myarray[1];
 
-            long[] myarray1;
+            Console.WriteLine("Result = {0}", myarray[0]);
+            Console.WriteLine("Result = {0}", myarray[1]);
+            Console.WriteLine("Result = {0}", myarray[2]);
 
-            myarray1 = new long[5];
+            long[] myarray1 = new long[3];
 
-            myarray1[2] = 7;
+            myarray1[0] = 7;
 
-            myarray1[4] = 12;
+            myarray1[1] = myarray1[0] * myarray1[0];
 
-            float[] myarray2;
+            myarray1[2] = myarray1[0] + myarray1[1];
 
-            myarray2 = new float[5];
+            Console.WriteLine("Result = {0}", myarray1[0]);
+            Console.WriteLine("Result = {0}", myarray1[1]);
+            Console.WriteLine("Result = {0}", myarray1[2]);
 
-            myarray2[3] = 3;
+            float[] myarray2 = new float[3];
 
-            myarray2[4] = 9;
+            myarray2[0] = 3;
+
+            myarray2[1] = myarray2[0] * myarray2[0];
+
+            myarray2[2] = myarray2[0] + myarray2[1];
 
                
 
-            Console.WriteLine ("Result = {0}", myarray[0]);
+            Console.WriteLine ("Result = {0}", myarray2[0]);
+            Console.WriteLine("Result = {0}", myarray2[1]);
+            Console.WriteLine("Result = {0}", myarray2[2]);
         }
     }
 }
